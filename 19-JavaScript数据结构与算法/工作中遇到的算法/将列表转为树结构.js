c274d1bef4bf57891e3f3337effdec07341a3f2a
王亮 <1392830517@qq.com>
2022-04-25 09:22:53 +0800

    feat: 将列表转成数

---
+++ 19-JavaScript数据结构与算法/工作中遇到的算法/将列表转为树结构.js
@@ -0,0 +1,48 @@
+let arr = [
+  {id: 1, name: '部门1', pid: 0},
+  {id: 2, name: '部门2', pid: 1},
+  {id: 3, name: '部门3', pid: 1},
+  {id: 4, name: '部门4', pid: 3},
+  {id: 5, name: '部门5', pid: 4},
+]
+
+/* 
+  分析:
+  1、id: 1, pid: 0 直接插入结果数组
+  2、id: 2, pid: 1, pid与结果数组中的第一项的id相等，则将该项插入到结果数组第一项的children属性中。
+  3、id: 3, pid: 1, pid与结果数组中的第一项的id相等，则将该项插入到结果数组第一项的children属性中。
+  4、id: 4, pid: 3, pid与
+*/
+
+
+function listToTree(list) {
+  let res = []
+  const hashMap = {}
+  for (let item of list) {
+    const id = item.id 
+    const pid = item.pid 
+    // 给每一项都添加children属性
+    // item.children = []
+
+    // 将该项也存入hashMap
+    hashMap[id] = item
+    if (pid === 0) {
+      // 如果pid为0时
+      res.push(item)
+    } else {
+      // 如果当前项pid在hashMap存在，则将该项插入到children中
+      if (hashMap[pid]) {
+        if (hashMap[pid].children) {
+          hashMap[pid].children.push(item)
+        } else {
+          hashMap[pid].children = [item]
+        }
+      }
+    }
+
+    
+  }
+
+  return res
+}
+
